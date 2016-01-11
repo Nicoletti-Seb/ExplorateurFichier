dc91c0413b6dd8739edaf990169db524ec5f5bd0
nourbenyas <benyas.nour@gmail.com>
2016-01-11 19:03:48 +0100

    tt

--- ExplorateurDeFichier/explorateur_plugins/src/main/java/fr/miage/m1/pa/explorateur_plugins/PluginVueCouleurFichier.java
+++ ExplorateurDeFichier/explorateur_plugins/src/main/java/fr/miage/m1/pa/explorateur_plugins/PluginVueCouleurFichier.java
@@ -23,64 +23,60 @@ public class PluginVueCouleurFichier implements Plugin {
 	public void plug(Controleur controleur) {
 
 		Modele modele = controleur.getModele();
-		
+
 		ChangeColorRight(modele);
-		
+
 	}
 
 	@Override
 	public void unplug(Controleur controleur) {
 		controleur.getModele().reset();
-		
+
 	}
 
 	@Override
 	public String getNom() {
 		return "VueCouleurFichier";
 	}
-	
-	private void ChangeColorRight(Modele modele){
+
+	private void ChangeColorRight(Modele modele) {
 		List<File> fileList = modele.getFileList();
 
+		JTable jtable = new JTable(tableModel);
+		JPanel jpanel = new JPanel();
+		JScrollPane scrollTable = new JScrollPane(jtable);
+		jpanel.add(scrollTable);
 
-	
-	JTable jtable= new JTable(tableModel);
-	JPanel jpanel =new JPanel();
-	JScrollPane scrollTable = new JScrollPane(jtable);
-	jpanel.add(scrollTable);
-	
-	for(int i = 0; i < fileList.size();i++) {
-		File f = fileList.get(i);
-		getNewRenderedTable(jtable, f);
-	
+		for (int i = 0; i < fileList.size(); i++) {
+			File f = fileList.get(i);
+			getNewRenderedTable(jtable, f);
+
+		}
+
 	}
-		
-	}
-	
+
 	private static JTable getNewRenderedTable(final JTable table, File f) {
 		table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
-			
+
 			@Override
-			public Component getTableCellRendererComponent(JTable table,
-					Object value, boolean isSelected, boolean hasFocus,int row, int col) {
-		
-					Component c = super.getTableCellRendererComponent(table, value, isSelected,hasFocus, row, col);
-						
-						if (f.canWrite()==false && f.canExecute()==false) {
-							c.setForeground(Color.red);
-						} else {
-							setBackground(table.getBackground());
-							setForeground(table.getForeground());
-						}
-					
-			
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+					boolean hasFocus, int row, int col) {
+
+				Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+
+				if (f.canWrite() == false && f.canExecute() == false) {
+					c.setForeground(Color.red);
+				} else {
+					setBackground(table.getBackground());
+					setForeground(table.getForeground());
+				}
 
 				return this;
 			}
 		});
 		return table;
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Build class");
 	}
